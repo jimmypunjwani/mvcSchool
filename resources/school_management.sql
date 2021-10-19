3c1110351f597ee5024982d4a9e3e04afb3ae736
Jimmy Punjwani <jimmy.punjwani@itrebp.org>
2021-10-19 18:51:54 +0500

    Database Added with Tables

---
+++ resources/school_management.sql
@@ -0,0 +1,38 @@
+CREATE TABLE [dbo].[Student] (
+    [StudentID]      INT           IDENTITY (1, 1) NOT NULL,
+    [LastName]       NVARCHAR (50) NOT NULL,
+    [FirstName]      NVARCHAR (50) NOT NULL,
+    [EnrollmentDate] DATETIME      NOT NULL,
+	[Middle Name]	 NVARCHAR (50) NULL,
+	[Hobby]			 NVARCHAR (50) NULL,
+    PRIMARY KEY CLUSTERED ([StudentID] ASC)
+)
+
+CREATE TABLE [dbo].[Course] (
+    [CourseID] INT           IDENTITY (1, 1) NOT NULL,
+    [Title]    NVARCHAR (50) NOT NULL,
+    [Credits]  INT           NOT NULL,
+    PRIMARY KEY CLUSTERED ([CourseID] ASC)
+)
+
+CREATE TABLE [dbo].[Lecturer] (
+    [LecturerID] 	INT           IDENTITY (1, 1) NOT NULL,
+    [First Name]    NVARCHAR (50) NOT NULL,
+    [Last Name]  	NVARCHAR (50) NOT NULL,
+    PRIMARY KEY CLUSTERED ([LecturerID] ASC)
+)
+
+CREATE TABLE [dbo].[Enrollment] (
+    [EnrollmentID] INT IDENTITY (1, 1) NOT NULL,
+    [Grade]        DECIMAL(3, 2) NULL,
+    [CourseID]     INT NOT NULL,
+    [StudentID]    INT NOT NULL,
+	[LecturerID]   INT NULL,
+    PRIMARY KEY CLUSTERED ([EnrollmentID] ASC),
+    CONSTRAINT [FK_dbo.Enrollment_dbo.Course_CourseID] FOREIGN KEY ([CourseID]) 
+        REFERENCES [dbo].[Course] ([CourseID]) ON DELETE CASCADE,
+    CONSTRAINT [FK_dbo.Enrollment_dbo.Student_StudentID] FOREIGN KEY ([StudentID]) 
+        REFERENCES [dbo].[Student] ([StudentID]) ON DELETE CASCADE,
+	CONSTRAINT [FK_dbo.Enrollment_dbo.Lecturer_LecturerID] FOREIGN KEY ([LecturerID]) 
+        REFERENCES [dbo].[Lecturer] ([LecturerID]) ON DELETE CASCADE
+)
